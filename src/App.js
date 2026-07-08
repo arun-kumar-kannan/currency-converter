b9b06a9095ab8996ec7bd2550fcd5d288d2df56e
Arunkumar <arunkumarnura18@gmail.com>
2026-07-08 19:07:38 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import CurrencyInput from "./CurrencyInput";
 
 class App extends Component {
+  state = {
+    from: "USD",
+    to: "INR",
+    rate: 1,
+    fromAmt: 1,
+    toAmt: 1,
+  };
+
+  setAmount = (amt, field) => {
+    if (field === "from") {
+      this.setState({
+        fromAmt: amt,
+        toAmt: null,
+      });
+    } else {
+      this.setState({
+        fromAmt: null,
+        toAmt: amt,
+      });
+    }
+  };
   render() {
-    return <div className="currency-converter"></div>;
+    return (
+      <div className="currency-converter">
+        <CurrencyInput
+          symbol={this.state.from}
+          selectSymbol={(sym) => this.setState({ from: sym })}
+          setAmount={(amt) => this.setState(amt, "from")}
+        />
+        <CurrencyInput
+          symbol={this.state.to}
+          selectSymbol={(sym) => this.setState({ to: sym })}
+          setAmount={(amt) => this.setState(amt, "to")}
+        />
+      </div>
+    );
   }
 }
 
---
+++ src/CurrencyInput.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const CurrencyInput = ({ symbol, selectSymbol, amount, setAmount }) => {
+  return (
+    <div className="field">
+      <div className="currency-picker">
+        <select value={symbol} onChange={(e) => selectSymbol(e.target.value)}>
+          <option>USD</option>
+          <option>INR</option>
+          <option>EUR</option>
+          <option>SGD</option>
+          <option>GBP</option>
+          <option>CAD</option>
+          <option>AUD</option>
+          <option>CNY</option>
+          <option>JPY</option>
+          <option>NZD</option>
+        </select>
+      </div>
+      <div className="currency-input">
+        <input
+          className="number-input"
+          type="number"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        ></input>
+      </div>
+    </div>
+  );
+};
+
+export default CurrencyInput;
